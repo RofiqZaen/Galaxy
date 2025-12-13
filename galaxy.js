4811046d05f677a76f028fdbb07912fa92fc84d0
Jasomaru <55294073+RofiqZaen@users.noreply.github.com>
2025-12-14 02:13:01 +0700

    galaxy.js

--- galaxy.js
+++ galaxy.js
@@ -1,24 +1,22 @@
 (function () {
     console.log("🔥 Remote script loaded");
 
-    // ❌ Kill switch
     if (!location.hostname.includes("mobstudio.ru")) {
         console.log("❌ Disabled on this domain");
         return;
     }
 
-    // ===== ERUDA =====
-    try {
-        if (!window.eruda) {
-            var s = document.createElement("script");
-            s.src = "https://cdn.jsdelivr.net/npm/eruda";
-            s.onload = function () {
-                eruda.init();
-                console.log("✅ ERUDA loaded");
-            };
-            document.body.appendChild(s);
-        }
-    } catch (e) {
-        console.error("❌ Galaxy error:", e);
+    if (!window.eruda) {
+        var s = document.createElement("script");
+        s.src = "https://cdn.jsdelivr.net/npm/eruda";
+        s.onload = function () {
+            eruda.init();
+            eruda.show(); // 👈 DI SINI
+            console.log("✅ ERUDA muncul");
+        };
+        document.body.appendChild(s);
+    } else {
+        // Kalau ERUDA sudah ada
+        eruda.show();
     }
 })();
